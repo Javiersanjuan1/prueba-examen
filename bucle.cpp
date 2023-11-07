7fe8b5ab9a04a4c23dad036a5397984c677dec16
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-07 11:17:07 +0100

    bucle

--- bucle.cpp
+++ bucle.cpp
@@ -9,9 +9,9 @@ int main() {
         for (int b = a; b <= fin; b++) {
             double c = sqrt(a * a + b * b);
 
-            // Verificar si los lados forman un triple pitagórico
+
             if (c == (int)c) {
-                // Verificar si cumplen las condiciones de un triángulo
+
                 if (a + b > c && a + c > b && b + c > a) {
                     printf("%d - %d - %.0lf\n", a, b, c);
                 }
